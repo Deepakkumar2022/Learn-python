da88c994221fe52b32bd5fb39a425e5cd817b77f
deepak <deepak.kumar@3scsolutions.com>
2025-04-25 23:05:05 +0530

    Task1 updated

---
+++ Basic Python Concepts.py
@@ -0,0 +1,17 @@
+# Task 1
+'''Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+Addition
+Subtraction
+Multiplication
+Division
+3.  Displays the results of each operation on the screen.'''
+
+Operand1 =int(input('Enter your first number: '))
+operand2 =int(input('Enter your second number: '))
+print('Addition:', Operand1+operand2) #Addition
+print('Subtraction:', Operand1-operand2) #Subtraction
+print('Multiplication', Operand1*operand2) #Multiplication
+print('Division', Operand1/operand2) #Division
+
