9f18bb1f35254ad8b156b1f41b74863c62fe2975
deepak <deepak.kumar@3scsolutions.com>
2025-04-29 18:10:04 +0530

    problem solved

---
+++ module2_Task1.py
@@ -0,0 +1,12 @@
+'''Task 1: Check if a Number is Even or Odd
+Problem Statement:  Write a Python program that:
+1. 	Takes an integer input from the user.
+2. 	Checks whether the number is even or odd using an if-else statement.
+3. 	Displays the result accordingly.'''
+
+Operand= int(input(('Enter your number: ')))
+if (Operand%2==0):
+    print('It is a even number')
+else:
+    print('It is an odd number')
+print('Thank you!')
